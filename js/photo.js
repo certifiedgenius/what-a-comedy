ff2d20c0148cab841d4ae9774d6b6c9d13769afd
Certified Genius <mugeera.mansoor@medieinstitutet.se>
2021-11-11 15:22:43 +0100

    GuavaJuice

---
+++ js/photo.js
@@ -0,0 +1,9 @@
+let galleryImages = document.querySelectorAll(".gallery-img");
+let getLatestOpenedImg;
+let windowWidth = window.innerWidth;
+
+if(galleryImages) {
+    galleryImages.forEach(function() {
+        
+    });
+}
