99d20e16dbfb1fc2bd29ec5237d7c6a2fc97b7fa
Certified Genius <mugeera.mansoor@medieinstitutet.se>
2021-11-11 21:28:08 +0100

    wut?

--- js/photo.js
+++ js/photo.js
@@ -1,3 +1,24 @@
+const fs = require('fs');
+const http = require('http');
+
+
+const json = fs.readFileSync(`${__dirname}/data/data.json`, 'utf-8');
+const imgData = JSON.parse(json);
+
+
+const server = http.createServer((req, res) => {
+    res.writeHead(200, { 'Content-type': 'text/html'});
+    res.end('This is the response');
+});
+
+
+server.listen(1337, '127.0.0.1', () => {
+    console.log('Listining for requessts now');
+});
+
+
+
+
 let galleryImages = document.querySelectorAll(".gallery-img");
 let getLatestOpenedImg;
 let windowWidth = window.innerWidth;
