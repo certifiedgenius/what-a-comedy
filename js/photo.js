ec8aa1b145a148f7ae38538e9b2e82753d7286ee
Certified Genius <mugeera.mansoor@medieinstitutet.se>
2021-11-11 16:00:40 +0100

    btn prev and next

--- js/photo.js
+++ js/photo.js
@@ -22,8 +22,19 @@ if(galleryImages) {
             newImgWindow.appendChild(newImg);
             newImg.setAttribute("src", "img/" + setNewImgUrl);
 
+            let newNextBtn = document.createElement("a");
+            let btnNextText = document.createTextNode("Next");
+            newNextBtn.appendChild(btnNextText);
+            container.appendChild(newNextBtn);
+            newNextBtn.setAttribute("class", "img-btn-next");
+            newNextBtn.setAttribute("onclick", "changeImg()");
+            
             let newPrevBtn = document.createElement("a");
             let btnPrevText = document.createTextNode("Prev");
+            newPrevBtn.appendChild(btnPrevText);
+            container.appendChild(newPrevBtn);
+            newPrevBtn.setAttribute("class", "img-btn-prev");
+            newPrevBtn.setAttribute("onclick", "changeImg()");
         }
     });
 }
