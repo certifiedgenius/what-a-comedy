881f515e963dccc1f7c9ca5d9f097e08a92d084f
Certified Genius <mugeera.mansoor@medieinstitutet.se>
2021-11-11 15:45:09 +0100

    popup window css and some js

--- js/photo.js
+++ js/photo.js
@@ -3,7 +3,20 @@ let getLatestOpenedImg;
 let windowWidth = window.innerWidth;
 
 if(galleryImages) {
-    galleryImages.forEach(function() {
-        
+    galleryImages.forEach(function(image, index) {
+        image.onclick = function() {
+            let getElementCss = window.getComputedStyle(image);
+            let getFullImgUrl = getElementCss.getPropertyValue("background-image");
+            let getImgUrlPos = getFullImgUrl.split("/img/thumbs/");
+            let setNewImgUrl = getImgUrlPos[1].replace('")', '');
+
+            getLatestOpenedImg = index + 1;
+
+            let container = document.body;
+            let newImgWindow = document.createElement("div");
+            container.appendChild(newImgWindow);
+            newImgWindow.setAttribute("class", "img-window");
+            newImgWindow.setAttribute("onclick", "closeImg()");
+        }
     });
 }
