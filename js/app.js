bd2c47cf601a2a712570cae42602b6fbf1543255
Certified Genius <mugeera.mansoor@medieinstitutet.se>
2021-11-24 22:22:44 +0100

    Fetch function

--- js/app.js
+++ js/app.js
@@ -7,5 +7,19 @@ fetch("/data/images.json")
     .then((photoData) => {
         photoData.forEach((image) => {
             let img = document.createElement("img");
+            let pTagg = document.createElement("p");
+            let pTaggAlt = document.createElement("p");
+
+            img.src = image.url;
+            img.alt = image.alt;
+            img.description = image.description;
+            img.dataset.description = image.description;
+            pTagg.innerHTML = image.title;
+            pTaggAlt.innerHTML = image.alt;
+            img.classList = "targetImage";
+
+            photoContainer.appendChild(img);
+            photoContainer.appendChild(pTagg);
+            photoContainer.appendChild(pTaggAlt);
         })
     })
