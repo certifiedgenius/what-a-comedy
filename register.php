f9ce7940c2b1dffa5b392cabbfeb1a4c27e183c9
certifiedgenius <mugeera.mansoor@medieinstitutet.se>
2022-03-15 20:40:02 +0100

    register site css fix

--- register.php
+++ register.php
@@ -6,37 +6,52 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-
-    <link rel="stylesheet" type="text/css" href="css/register.css">
+    <!-- Custom CSS File's -->
+    <link rel="stylesheet" type="text/css" href="css/main.css">
 
 
     <title>Register</title>
+
 </head>
 
+
 <body>
-    <div class="register-container">
-        <h1 class="h1-register">Sign up for what a comedy</h1>
-        <form class="form-register" action="./admin/register_user.php" method="POST">
-            <div class="div-register">
-                <label><span>E-mail</span>
-                    <input class="input-user" id="email" type="email" name="email" required />
+
+    <div class="wrapper-login">
+
+        <h1>Sign up for what a comedy</h1>
+
+        <form class="form-login" action="./admin/register_user.php" method="POST">
+
+            <div>
+                <label>
+
+                    <input class="input-user input-login" id="email" type="email" name="email" placeholder="E-mail" required>
                 </label>
             </div>
 
-            <div class="div-register">
-                <label><span>Password</span>
-                    <input class="input-user" type="password" name="password" required />
+
+            <div>
+                <label>
+
+                    <input class="input-user input-login" type="password" name="password" placeholder="Password" required>
                 </label>
             </div>
-            <button class="register-btn" type="submit" name="loginform">Create</button>
+
+            <button class="button" type="submit" name="loginform">Create</button>
         </form>
-        <div class="div-tagg">
+
+
+
+
+        <div>
             Already have an account?<a href="login.php">
-                <p class="text-info">Login here</p>
+
+                <p class="button">Log in here</p>
             </a>
         </div>
-    </div>
-    
-</body>
 
+    </div>
+
+</body>
 </html>
